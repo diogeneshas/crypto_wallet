380dde57da5fa918d069373eab4a015a14948b4c
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-26 08:17:45 -0300

    feat: Criando o segundo CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cadastrando moedas.....'
+
+Coin.create!([
+
+  { description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png' },
+
+  { description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png' },
+
+  { description: 'Dash',
+  acronym: 'Dash',
+  url_image: 'https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-3.png' }
+
+])
+
+puts 'Moedas cadastradas com sucesso'
+
+puts 'Cadastrando tipos de mineração.....'
+
+MiningType.create!([
+  { name: "Proof of Work", acronym: "PoW" },
+  { name: "Proof of Stake", acronym: "PoS" },
+  { name: "Proof of Capacity", acronym: "PoC" }
+])
+
+puts 'Tipos de mineração cadastradas com sucesso'
