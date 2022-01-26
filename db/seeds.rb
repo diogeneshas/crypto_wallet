ea15256bda92a6cefdc83023b93a9fb3d757a3e8
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-26 08:57:41 -0300

    feat: Ajustando o seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Cadastrando moedas.....'
-
-Coin.create!([
-
-  { description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png' },
-
-  { description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png' },
-
-  { description: 'Dash',
-  acronym: 'Dash',
-  url_image: 'https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-3.png' }
-
-])
-
-puts 'Moedas cadastradas com sucesso'
-
 puts 'Cadastrando tipos de mineração.....'
 
 MiningType.create!([
@@ -35,3 +15,26 @@ MiningType.create!([
 ])
 
 puts 'Tipos de mineração cadastradas com sucesso'
+
+puts 'Cadastrando moedas.....'
+
+Coin.create!([
+
+  { description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://logosmarcas.net/wp-content/uploads/2020/08/Bitcoin-Logo.png',
+  mining_type: MiningType.all.sample },
+
+  { description: 'Ethereum',
+  acronym: 'ETH',
+  url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/600px-ETHEREUM-YOUTUBE-PROFILE-PIC.png',
+  mining_type: MiningType.all.sample },
+
+  { description: 'Dash',
+  acronym: 'Dash',
+  url_image: 'https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-3.png',
+  mining_type: MiningType.all.sample },
+
+])
+
+puts 'Moedas cadastradas com sucesso'
