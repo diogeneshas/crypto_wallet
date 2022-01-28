832c5bded6a63ca3d42b284319173036fbe95047
diogeneshas <diogenes.henrique.br@gmail.com>
2022-01-28 08:11:40 -0300

    feat: Ativando I18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
+
+  def locale(locale)
+    I18n.locale == :en ? "Estados Unidos" : "Portugues do Brasil"
+  end
 end
